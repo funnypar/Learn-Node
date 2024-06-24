0745989edba230efb3e95692a875ce788cfd70ad
Mopano <99732517+funnypar@users.noreply.github.com>
2024-06-24 15:52:16 +0330

    postTour

--- 05-Natours/server.js
+++ 05-Natours/server.js
@@ -5,6 +5,7 @@ const express = require('express');
 const PORT = 8000;
 // Create app
 const app = express();
+app.use(express.json());
 
 // Read Files
 const tours = JSON.parse(
@@ -20,6 +21,30 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        res.status(424).send('Something went wrong ...');
+      } else {
+        res.status(201).json({
+          status: 'success',
+          data: {
+            tours,
+          },
+        });
+      }
+    }
+  );
 });
 
 // Listen to the server
