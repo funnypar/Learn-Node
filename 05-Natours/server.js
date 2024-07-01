396b7eea5b8cb54e0941b7d2b63a542bcb0fef19
Mopano <99732517+funnypar@users.noreply.github.com>
2024-07-01 15:06:40 +0330

    addedSchema

--- 05-Natours/server.js
+++ 05-Natours/server.js
@@ -22,6 +22,25 @@ mongoose
   })
   .then(() => console.log('DB Is Connected!'));
 
+// schema
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: [true, 'The Name Must Be Insert.'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'The Price Must Be Insert.'],
+  },
+  raiting: {
+    type: Number,
+    default: 4.5,
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 // Listen to the server
 app.listen(PORT, '127.0.0.1', () => {
   console.log(`Server is running on port: ${PORT}... `);
