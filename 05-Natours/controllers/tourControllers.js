c9606e71402f540aecadd8fb49eff189892c1321
Mopano <99732517+funnypar@users.noreply.github.com>
2024-07-03 16:48:42 +0330

    getAllTours

--- 05-Natours/controllers/tourControllers.js
+++ 05-Natours/controllers/tourControllers.js
@@ -1,24 +1,40 @@
 const Tour = require('../models/tourModel');
 
-exports.getAllTours = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    // results: tours.length,
-    // data: {
-    //   tours,
-    // },
-  });
+exports.getAllTours = async (req, res) => {
+  try {
+    const tours = await Tour.find();
+
+    res.status(200).json({
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      data: 'Something went wrong...',
+    });
+  }
 };
 
-exports.getOneTour = (req, res) => {
-  // const tour = tours.find((el) => el.id === +req.params.id);
+exports.getOneTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
 
-  res.status(200).json({
-    status: 'success',
-    // data: {
-    //   tour,
-    // },
-  });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      data: 'Something went wrong...',
+    });
+  }
 };
 
 exports.createTour = async (req, res) => {
@@ -33,7 +49,7 @@ exports.createTour = async (req, res) => {
   } catch (err) {
     res.status(400).json({
       status: 'fail',
-      massege: err,
+      massege: 'Something went wrong...',
     });
   }
 };
