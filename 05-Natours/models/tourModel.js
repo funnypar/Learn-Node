4e1ae8bb520d44599ac93f245186fad5160c2641
Mopano <99732517+funnypar@users.noreply.github.com>
2024-07-11 13:39:40 +0330

    virtualProperty

--- 05-Natours/models/tourModel.js
+++ 05-Natours/models/tourModel.js
@@ -1,60 +1,70 @@
 const mongoose = require('mongoose');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
-    required: [true, 'The Name Must Be Insert.'],
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      unique: true,
+      required: [true, 'The Name Must Be Insert.'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'The Price Must Be Insert.'],
+    },
+    duration: {
+      type: Number,
+      required: [true, 'The Duration Must Be Insert.'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'The Difficulty Must Be Insert.'],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 4.5,
+      required: [true, 'The Raiting Must Be Insert.'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4,
+    },
+    images: [String],
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'The Group size Must Be Insert.'],
+    },
+    sammary: {
+      type: String,
+      trim: true,
+      required: [true, 'The Sammary Must Be Insert.'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      trim: true,
+      required: [true, 'The Image Must Be Insert.'],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
   },
-  price: {
-    type: Number,
-    required: [true, 'The Price Must Be Insert.'],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
   },
-  duration: {
-    type: Number,
-    required: [true, 'The Duration Must Be Insert.'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'The Difficulty Must Be Insert.'],
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 4.5,
-    required: [true, 'The Raiting Must Be Insert.'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4,
-  },
-  images: [String],
-  startDates: [Date],
-  secretTour: {
-    type: Boolean,
-    default: false,
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'The Group size Must Be Insert.'],
-  },
-  sammary: {
-    type: String,
-    trim: true,
-    required: [true, 'The Sammary Must Be Insert.'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    trim: true,
-    required: [true, 'The Image Must Be Insert.'],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-  },
+);
+
+tourSchema.virtual('durationWeeks').get(function () {
+  return this.duration / 7;
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
