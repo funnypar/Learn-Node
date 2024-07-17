ea32065c844dd6ebd485dcccd10b4ab91ed16dab
Mopano <99732517+funnypar@users.noreply.github.com>
2024-07-17 16:08:24 +0330

    errorDevAndProd

--- 05-Natours/controllers/errorControllers.js
+++ 05-Natours/controllers/errorControllers.js
@@ -1,9 +1,37 @@
+const resDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+
+const resProduct = (err, res) => {
+  if (err.isOperational) {
+    // Operational, trusted error
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // 1) Log error
+    console.log('Error 💥', err);
+    // Programming or other unknown error: Do NOT leak error
+    res.status(500).json({
+      status: err.status,
+      message: 'Something went wrong...',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    resDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    resProduct(err, res);
+  }
 };
